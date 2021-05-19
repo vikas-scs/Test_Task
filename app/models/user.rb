f248c419199ce2c4782109b0e8df4ad38b7e7ddf
vikas-scs <vikas@sykam.org>
2021-05-19 19:35:46 +0530

    email added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
          has_many :projects
    validates :mobile, presence: :true
    validates :first_name, presence: :true, format: { with: /\A[a-zA-Z]+\z/}
